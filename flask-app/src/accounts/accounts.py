e9a8d63cfe8d843c6f63f858d7ae8e574f0bd73a
Sena Szczepaniuk <senaszczepaniuk@Senas-MacBook-Air.local>
2023-04-17 17:55:47 -0400

    routes and tests

--- flask-app/src/accounts/accounts.py
+++ flask-app/src/accounts/accounts.py
@@ -30,10 +30,11 @@ def post_accounts():
     cursor.execute(query)
 
 # Gets all account data for a given username
-@accounts.route('/accounts/{username}', methods=['GET'])
+@accounts.route('/accounts/<username>', methods=['GET'])
 def get_account(username):
     cursor = db.get_db().cursor()
-    query = 'select * from Accounts where username =' + username
+    query = 'select * from Accounts where username = "{}"'.format(username)
+    #return jsonify(query)
     cursor.execute(query)
     column_headers = [x[0] for x in cursor.description]
     json_data = []
@@ -48,7 +49,7 @@ def get_account(username):
 @accounts.route('/accounts/{username}', methods=['DELETE'])
 def delete_account(username):
     cursor = db.get_db().cursor()
-    query = 'delete * from Accounts where username =' + username
+    query = 'delete * from Accounts where username ="{}"'.format(username)
     cursor.execute(query)
     column_headers = [x[0] for x in cursor.description]
     json_data = []
@@ -62,14 +63,14 @@ def delete_account(username):
 @accounts.route('/accounts/{pword}', methods=['GET'])
 def get_account_pword(username):
     cursor = db.get_db().cursor()
-    query = 'select pword from Accounts where username =' + username
+    query = 'select pword from Accounts where username ="{}"'.format(username)
     cursor.execute(query)
 
 # Updates the password of the given account
 @accounts.route('/accounts/{pword}', methods=['POST'])
 def update_account_pword(username, pword):
     cursor = db.get_db().cursor()
-    query = 'update Accounts set pword =' + pword + ' where username =' + username
+    query = 'update Accounts set pword ="{}" where username ="{}"'.format(pword, username)
     cursor.execute(query)
     column_headers = [x[0] for x in cursor.description]
     json_data = []
@@ -83,7 +84,7 @@ def update_account_pword(username, pword):
 @accounts.route('/accounts/{bio}', methods=['GET'])
 def get_account_bio(username):
     cursor = db.get_db().cursor()
-    query = 'select bio from Accounts where username = ' + username
+    query = 'select bio from Accounts where username = "{}"'.format(username)
     cursor.execute(query)
     column_headers = [x[0] for x in cursor.description]
     json_data = []
@@ -97,7 +98,7 @@ def get_account_bio(username):
 @accounts.route('/accounts/{bio}', methods=['POST'])
 def update_account_bio(bio, username):
     cursor = db.get_db().cursor()
-    query = 'update Accounts set bio =' + bio + ' where username =' + username
+    query = 'update Accounts set bio ="{}" where username ="{}"'.format(bio, username)
     cursor.execute(query)
     column_headers = [x[0] for x in cursor.description]
     json_data = []
@@ -111,7 +112,7 @@ def update_account_bio(bio, username):
 @accounts.route('/accounts/{bio}', methods=['DELTE'])
 def delete_accounts_bio(username):
     cursor = db.get_db().cursor()
-    query = 'update Accounts set bio = "" where username =' + username
+    query = 'update Accounts set bio = "" where username ="{}"'.format(username)
     cursor.execute(query)
     column_headers = [x[0] for x in cursor.description]
     json_data = []
