52c981864680ef0f035b0d1ee8a55eea249ffbbb
Nagi Aboushaca <nagiaboushaca@Nagis-MacBook-Pro.local>
2023-04-20 18:56:00 -0400

    josie and brock

--- flask-app/src/brock/brock.py
+++ flask-app/src/brock/brock.py
@@ -21,17 +21,12 @@ def get_recipes():
 
     return jsonify(json_data)
 
-<<<<<<< HEAD
-# Get all the details of a recipe a given post
-@brock.route('/recipeFromPost', methods=['GET'])
-=======
 # Get all the details of a given recipe
-@brock.route('/recipeSpecific', methods=['GET'])
->>>>>>> 7ccab5b96ede7efb831b31c64b4ea8ca805226d8
-def get_recipe():
-    the_data = request.json
-    current_app.logger.info(the_data)
-    postid = the_data['post_id_json']
+@brock.route('/recipeFromPost/<postid>', methods=['GET'])
+def get_recipe(postid):
+    # the_data = request.json
+    # current_app.logger.info(the_data)
+    # postid = the_data['post_id_json']
     cursor = db.get_db().cursor()
     query = 'select * from Recipes where recipe_id ={}'.format(postid)
     cursor.execute(query)
