427eda6ccf0b94819015ccd228ae5ee9c91db62b
Sena Szczepaniuk <szczepaniuk.s@northeastern.edu>
2023-04-20 13:51:43 -0400

    Auto Increment

--- db/db_bootstrap.sql
+++ db/db_bootstrap.sql
@@ -42,26 +42,29 @@ CREATE TABLE IF NOT EXISTS FollowerRelationship (
 );
 
 CREATE TABLE IF NOT EXISTS Posts (
-    post_id int PRIMARY KEY,
+    post_id int NOT NULL AUTO_INCREMENT,
     poster varchar(20) UNIQUE NOT NULL,
     caption varchar(750),
     post_time DATETIME DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (post_id),
     CONSTRAINT username_reference
         FOREIGN KEY (poster) REFERENCES Accounts (username)
 );
 
 CREATE TABLE IF NOT EXISTS Tags (
-    tag_id int PRIMARY KEY,
+    tag_id int NOT NULL AUTO_INCREMENT,
     post_id int NOT NULL,
     tag_name varchar(20),
+    PRIMARY KEY (tag_id),
     CONSTRAINT tag_reference
         FOREIGN KEY (post_id) REFERENCES Posts (post_id)
 );
 
 CREATE TABLE IF NOT EXISTS Likes (
-    like_id int PRIMARY KEY,
+    like_id int NOT NULL AUTO_INCREMENT,
     liker varchar(20) NOT NULL,
     post_id int NOT NULL,
+    PRIMARY KEY (like_id),
     CONSTRAINT username_reference2
         FOREIGN KEY (liker) REFERENCES Accounts (username),
     CONSTRAINT post_id_reference
@@ -69,11 +72,12 @@ CREATE TABLE IF NOT EXISTS Likes (
 );
 
 CREATE TABLE IF NOT EXISTS Comments (
-    comment_id int PRIMARY KEY,
+    comment_id int NOT NULL AUTO_INCREMENT,
     commenter varchar(20) NOT NULL,
     post_id int NOT NULL,
     content varchar(200),
     comment_time DATETIME DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (comment_id),
     CONSTRAINT username_reference3
         FOREIGN KEY (commenter) REFERENCES Accounts (username),
     CONSTRAINT post_id_reference2
@@ -81,17 +85,19 @@ CREATE TABLE IF NOT EXISTS Comments (
 );
 
 CREATE TABLE IF NOT EXISTS Skill_Level (
-    skill_level_id int PRIMARY KEY,
-    skill_name varchar(20)
+    skill_level_id int NOT NULL AUTO_INCREMENT,
+    skill_name varchar(20),
+    PRIMARY KEY (skilL_level_id)
 );
 
 CREATE TABLE IF NOT EXISTS Cuisines (
-    cuisine_id int PRIMARY KEY,
-    cuisine_name varchar(20)
+    cuisine_id int NOT NULL AUTO_INCREMENT,
+    cuisine_name varchar(20),
+    PRIMARY KEY (cuisine_id)
 );
 
 CREATE TABLE IF NOT EXISTS Recipes (
-    recipe_id int PRIMARY KEY,
+    recipe_id int NOT NULL AUTO_INCREMENT,
     recipe_name varchar(30),
     steps varchar(16000),
     recipe_time int NOT NULL,
@@ -99,6 +105,7 @@ CREATE TABLE IF NOT EXISTS Recipes (
     serving_size int NOT NULL,
     calories int,
     cuisine_id int NOT NULL,
+    PRIMARY KEY (recipe_id),
     CONSTRAINT recipe_post_reference
         FOREIGN KEY (recipe_id) REFERENCES Posts (post_id),
     CONSTRAINT skill_level_reference
@@ -108,8 +115,9 @@ CREATE TABLE IF NOT EXISTS Recipes (
 );
 
 CREATE TABLE IF NOT EXISTS Ingredient_Category (
-    category_id int PRIMARY KEY,
-    category_name varchar(50)
+    category_id int NOT NULL AUTO_INCREMENT,
+    category_name varchar(50),
+    PRIMARY KEY (category_id)
 );
 
 CREATE TABLE IF NOT EXISTS Ingredients (
@@ -168,113 +176,99 @@ INSERT INTO FollowerRelationship
 VALUES ('wcolsey','sena.szczepaniuk');
 
 
-INSERT INTO Posts
-VALUES (1, 'nagiaboushaca',
+INSERT INTO Posts (poster, caption, post_time)
+VALUES ('nagiaboushaca',
         'I wanted to share this pasta recipe with you guys! Perfect for a quick lunch!',
         CURRENT_TIMESTAMP);
-INSERT INTO Posts
-VALUES (2, 'wcolsey',
+INSERT INTO Posts (poster, caption, post_time)
+VALUES ('wcolsey',
         'I LOVE making Beef Stroganoff, so I wanted to shore with you all my *secret!* family recipe :)',
         CURRENT_TIMESTAMP);
-INSERT INTO Posts
-VALUES (3, 'sena.szczepaniuk',
+INSERT INTO Posts (poster, caption, post_time)
+VALUES ('sena.szczepaniuk',
         'PB&J for hard times',
         CURRENT_TIMESTAMP);
 
 
-INSERT INTO Tags
-VALUES (1, 1, '#cheesy');
-INSERT INTO Tags
-VALUES (2, 1, '#quicklunch');
-INSERT INTO Tags
-VALUES (3, 2, '#hearty');
-INSERT INTO Tags
-VALUES (4, 2, '#juicy');
-INSERT INTO Tags
-VALUES (5, 3, '#depressionmeal');
-INSERT INTO Tags
-VALUES (6, 3, '#college');
+INSERT INTO Tags (post_id, tag_name)
+VALUES (1, '#cheesy');
+INSERT INTO Tags (post_id, tag_name)
+VALUES (1, '#quicklunch');
+INSERT INTO Tags (post_id, tag_name)
+VALUES (2, '#hearty');
+INSERT INTO Tags (post_id, tag_name)
+VALUES (2, '#juicy');
+INSERT INTO Tags (post_id, tag_name)
+VALUES (3, '#depressionmeal');
+INSERT INTO Tags (post_id, tag_name)
+VALUES (3, '#college');
 
 
 -- Post_id: 1, Num_Likes: 2
 -- Post_id: 2, Num_Likes: 2
 -- Post_id: 3, Num_Likes: 1
-INSERT INTO Likes
-VALUES (1,
-        'nagiaboushaca',
-        1);
-INSERT INTO Likes
-VALUES (2,
-        'nagiaboushaca',
-        2);
-INSERT INTO Likes
-VALUES (3,
-        'nagiaboushaca',
-        3);
-INSERT INTO Likes
-VALUES (4,
-        'wcolsey',
-        2);
-INSERT INTO Likes
-VALUES (5,
-        'sena.szczepaniuk',
-        1);
+INSERT INTO Likes (liker, post_id)
+VALUES ('nagiaboushaca', 1);
+INSERT INTO Likes (liker, post_id)
+VALUES ('nagiaboushaca', 2);
+INSERT INTO Likes (liker, post_id)
+VALUES ('nagiaboushaca', 3);
+INSERT INTO Likes (liker, post_id)
+VALUES ('wcolsey', 2);
+INSERT INTO Likes (liker, post_id)
+VALUES ('sena.szczepaniuk', 1);
 
 
 -- Post_id: 1, Num_Comments: 2
 -- Post_id: 2, Num_Comments: 1
 -- Post_id: 3, Num_Comments: 1
-INSERT INTO Comments
-VALUES (1,
-        'nagiaboushaca',
+INSERT INTO Comments (commenter, post_id, content, comment_time)
+VALUES ('nagiaboushaca',
         1,
         'Like and follow for more content!',
         CURRENT_TIMESTAMP);
-INSERT INTO Comments
-VALUES (2,
-        'wcolsey',
+INSERT INTO Comments (commenter, post_id, content, comment_time)
+VALUES ('wcolsey',
         3,
         'pb&j never really whetted my tastebuds, but this is rad!!',
         CURRENT_TIMESTAMP);
-INSERT INTO Comments
-VALUES (3,
-        'sena.szczepaniuk',
+INSERT INTO Comments (commenter, post_id, content, comment_time)
+VALUES ('sena.szczepaniuk',
         2,
         'Nice Beef Stroganoff',
         CURRENT_TIMESTAMP);
-INSERT INTO Comments
-VALUES (4,
-        'sena.szczepaniuk',
+INSERT INTO Comments (commenter, post_id, content, comment_time)
+VALUES ('sena.szczepaniuk',
         1,
         'I dont really like pasta tbh',
         CURRENT_TIMESTAMP);
 
 
-INSERT INTO Skill_Level
-VALUES (1, 'Beginner');
-INSERT INTO Skill_Level
-VALUES (2, 'Intermediate');
-INSERT INTO Skill_Level
-VALUES (3, 'Amateur');
-INSERT INTO Skill_Level
-VALUES (4, 'Advanced');
-INSERT INTO Skill_Level
-VALUES (5, 'Professional Chef');
+INSERT INTO Skill_Level (skill_name)
+VALUES ('Beginner');
+INSERT INTO Skill_Level (skill_name)
+VALUES ('Intermediate');
+INSERT INTO Skill_Level (skill_name)
+VALUES ('Amateur');
+INSERT INTO Skill_Level (skill_name)
+VALUES ('Advanced');
+INSERT INTO Skill_Level (skill_name)
+VALUES ('Professional Chef');
 
 
-INSERT INTO Cuisines
-VALUES (1, 'Asian');
-INSERT INTO Cuisines
-VALUES (2, 'Caribbean');
-INSERT INTO Cuisines
-VALUES (3, 'Mediterrean');
-INSERT INTO Cuisines
-VALUES (4, 'Fusion');
-INSERT INTO Cuisines
-VALUES (5, 'New American');
+INSERT INTO Cuisines (cuisine_name)
+VALUES ('Asian');
+INSERT INTO Cuisines (cuisine_name)
+VALUES ('Caribbean');
+INSERT INTO Cuisines (cuisine_name)
+VALUES ('Mediterrean');
+INSERT INTO Cuisines (cuisine_name)
+VALUES ('Fusion');
+INSERT INTO Cuisines (cuisine_name)
+VALUES ('New American');
 
-INSERT INTO Recipes
-VALUES (1, 'Broccoli Cheddar Pasta',
+INSERT INTO Recipes (recipe_name, steps, recipe_time, skill_level_id, serving_size, calories, cuisine_id)
+VALUES ('Broccoli Cheddar Pasta',
         '1. Salt some water and bring to a boil in a pot, and boil the pasta
          2. Wash and cut your broccoli crown into bite size pieces
          3. Stir-fry broccoli in a pan with oil
@@ -287,8 +281,8 @@ VALUES (1, 'Broccoli Cheddar Pasta',
         300,
         5);
 
-INSERT INTO Recipes
-VALUES (2, 'Beef Stroganoff',
+INSERT INTO Recipes (recipe_name, steps, recipe_time, skill_level_id, serving_size, calories, cuisine_id)
+VALUES ('Beef Stroganoff',
         '1. Sear 2 batches of beef, 1 minute per side on high heat
          2. Add butter and onions and mushrooms, and sautee for 6-8 minutes
          3. Add one minced garlic clove and sautee for one minute
@@ -301,8 +295,8 @@ VALUES (2, 'Beef Stroganoff',
         689,
         5);
 
-INSERT INTO Recipes
-VALUES (3, 'PB&J Sandwich',
+INSERT INTO Recipes (recipe_name, steps, recipe_time, skill_level_id, serving_size, calories, cuisine_id)
+VALUES ('PB&J Sandwich',
         '1. Toast bread for 3 minutes
          2. Spread peanut butter on one piece of bread
          3. Spread jelly on the other piece of bread
@@ -314,24 +308,24 @@ VALUES (3, 'PB&J Sandwich',
         5);
 
 
-INSERT INTO Ingredient_Category
-VALUES (1, 'Fruits');
-INSERT INTO Ingredient_Category
-VALUES (2, 'Vegetables');
-INSERT INTO Ingredient_Category
-VALUES (3, 'Meat');
-INSERT INTO Ingredient_Category
-VALUES (4, 'Seafood');
-INSERT INTO Ingredient_Category
-VALUES (5, 'Grains, nuts, and baking products');
-INSERT INTO Ingredient_Category
-VALUES (6, 'Seasonings');
-INSERT INTO Ingredient_Category
-VALUES (7, 'Dairy');
-INSERT INTO Ingredient_Category
-VALUES (8, 'Fats and oils');
-INSERT INTO Ingredient_Category
-VALUES (9, 'Other');
+INSERT INTO Ingredient_Category (category_name)
+VALUES ('Fruits');
+INSERT INTO Ingredient_Category (category_name)
+VALUES ('Vegetables');
+INSERT INTO Ingredient_Category (category_name)
+VALUES ('Meat');
+INSERT INTO Ingredient_Category (category_name)
+VALUES ('Seafood');
+INSERT INTO Ingredient_Category (category_name)
+VALUES ('Grains, nuts, and baking products');
+INSERT INTO Ingredient_Category (category_name)
+VALUES ('Seasonings');
+INSERT INTO Ingredient_Category (category_name)
+VALUES ('Dairy');
+INSERT INTO Ingredient_Category (category_name)
+VALUES ('Fats and oils');
+INSERT INTO Ingredient_Category (category_name)
+VALUES ('Other');
 
 
 INSERT INTO Ingredients
@@ -377,3 +371,4 @@ INSERT INTO Saved_recipes
 VALUES ('wcolsey', 1);
 INSERT INTO Saved_recipes
 VALUES ('sena.szczepaniuk', 3);
+
--- flask-app/src/brock/brock.py
+++ flask-app/src/brock/brock.py
@@ -2,7 +2,6 @@ from flask import Blueprint, request, jsonify, make_response, current_app
 import json
 from src import db
 
-
 brock = Blueprint('brock', __name__)
 
 # Get all recipes from the database
